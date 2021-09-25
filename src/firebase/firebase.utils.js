6ff503bbe1893b3a470d74c6e248c3c59d0f64dc
Raymond <coderdeluxe@gmail.com>
2021-09-25 19:36:21 +0800

    added redux implementation

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -28,15 +28,15 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
-    } catch(error) {
-      console.log('error creating user', error.message)
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
   }
 
   return userRef;
-}
+};
 
 firebase.initializeApp(config);
 
@@ -44,8 +44,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account'});
+provider.setCustomParameters({ prompt: "select_account" });
 
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = () =>
+  auth.signInWithPopup(provider).catch(() => {});
 
 export default firebase;
