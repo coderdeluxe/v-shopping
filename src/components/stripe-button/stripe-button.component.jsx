e2ec548b5d118d141bea0b283f3111104e25a7f8
Raymond <coderdeluxe@gmail.com>
2021-10-02 14:49:02 +0800

    Updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51H0eBCCHDCav7mPQWRjeFug1O7rmntJf5tUXFv5ZqBaZCtwibmdk4P1Zl1uwx4Sdkws9Jlqe7jUC41r56TPwb3PZ00zEhAR35T";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Virtual Shopping Ltd."
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
